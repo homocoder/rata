1d22e56d70204dd38db1f023c7d53c5add50bed8
Fernando Morales <fernando@fmorales.cl>
2021-12-23 14:07:57 -0300

    sync

--- src/forecasts_binclf_launcher.bash
+++ src/forecasts_binclf_launcher.bash
@@ -1,5 +1,5 @@
 interval=$1
-sleep 15
+sleep 10
 cd /home/selknam/dev/rata/src && \
   source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
   python /home/selknam/dev/rata/src/forecasts_binclf_launcher.py \
