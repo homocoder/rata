24fd479f7af53d3bd3d7eacd4c804e577cff40b3
Fernando Morales <fernando@fmorales.cl>
2021-12-28 10:45:31 -0300

    sync

--- src/models_binclf_launcher.olds.tmp.bash
+++ src/models_binclf_launcher.olds.tmp.bash
@@ -7,24 +7,6 @@ cd /home/selknam/dev/rata/src && \
     --db_conf=conf/db.json \
     --symbol_conf=conf/rates_launcher.$interval.json \
     --models_binclf_conf=conf/models_binclf.json \
-    --model_datetime=2021-12-26T21:00:00
-    
-cd /home/selknam/dev/rata/src && \
-  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
-  timeout 1740 \
-  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
-    --db_conf=conf/db.json \
-    --symbol_conf=conf/rates_launcher.$interval.json \
-    --models_binclf_conf=conf/models_binclf.json \
-    --model_datetime=2021-12-26T21:30:00 
-
-cd /home/selknam/dev/rata/src && \
-  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
-  timeout 1740 \
-  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
-    --db_conf=conf/db.json \
-    --symbol_conf=conf/rates_launcher.$interval.json \
-    --models_binclf_conf=conf/models_binclf.json \
     --model_datetime=2021-12-27T10:00:00 
 
 cd /home/selknam/dev/rata/src && \
