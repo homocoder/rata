46be1290444f4fbe28482af1fcff3be573cd1d0f
Fernando Morales <fernando@fmorales.cl>
2021-12-28 10:19:57 -0300

    sync

---
+++ src/models_binclf_launcher.olds.tmp.bash
@@ -0,0 +1,129 @@
+interval=5
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-26T21:00:00
+    
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-26T21:30:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T10:00:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T10:30:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T11:00:00
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T11:30:00
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T19:00:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T19:30:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T23:00:00
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-27T23:30:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-28T:09:00:00 
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-28T09:30:00
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-28T11:00:00
+
+cd /home/selknam/dev/rata/src && \
+  source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
+  timeout 1740 \
+  python /home/selknam/dev/rata/src/models_binclf_launcher.py \
+    --db_conf=conf/db.json \
+    --symbol_conf=conf/rates_launcher.$interval.json \
+    --models_binclf_conf=conf/models_binclf.json \
+    --model_datetime=2021-12-28T11:30:00
+
+  # this is the way
