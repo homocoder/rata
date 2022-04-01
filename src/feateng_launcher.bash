a8b31497ba2411fc38daead48fed3385076336e3
Fernando Morales <fernando@fmorales.cl>
2022-04-01 15:35:35 -0300

    sync

--- src/feateng_launcher.bash
+++ src/feateng_launcher.bash
@@ -1,5 +1,6 @@
-sleep 26
+sleep 20
 cd /home/selknam/var && \
   source /home/selknam/opt/miniconda3/bin/activate rata.py39 && \
   time python -u /home/selknam/dev/rata/src/feateng.py
+  time python -u /home/selknam/dev/rata/src/feateng_2.py
 date
