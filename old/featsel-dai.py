e145255178b1993904145f1f35feec30708d3b8e
Fernando Morales <fernando@fmorales.cl>
2022-07-17 17:42:06 +0000

    sync

---
+++ old/featsel-dai.py
@@ -0,0 +1,123 @@
+# %% 🐭
+from sys import argv
+from rata.utils import lstm_prep, parse_argv
+
+fake_argv = 'featsel.py --db_host=localhost --symbol=AUDUSD --kind=forex --interval=3 --nrows=6000'
+fake_argv = fake_argv.split()
+argv = fake_argv #### *!
+_conf = parse_argv(argv=argv)
+_conf
+
+# %%
+# Global imports
+import pandas as pd
+from rata.ratalib import check_time_gaps
+
+#%%
+from sqlalchemy import create_engine
+engine = create_engine('postgresql+psycopg2://rata:acaB.1312@localhost:5432/rata')
+
+#symbols = ['AUDUSD', 'GBPAUD', 'AUDCHF', 'GBPNZD', 'AUDNZD', 'EURGBP', 'NZDUSD'] 
+symbols = ['AUDUSD', 'AUDCHF', 'NZDUSD']
+
+df_join = pd.DataFrame()
+for s in symbols:
+    sql =  "select * from feateng "
+    sql += "where symbol='" + s + "' and interval=" + str(_conf['interval'])
+    sql += " order by tstamp desc limit " + str(_conf['nrows'])
+    print(sql)
+    df = pd.read_sql_query(sql, engine).sort_values('tstamp')
+    X_prefix = s + '_' + str(_conf['interval']) + '_'
+    for c in df.columns:
+        df.rename({c: X_prefix + c}, axis=1, inplace=True)
+    
+    if len(df_join) == 0:
+        df_join = df
+    else:
+        df_join = pd.merge(df_join, df, how='inner', left_on=df_join.columns[0], right_on=df.columns[0])
+df = df_join.copy()
+df.sort_values(df.columns[0])
+df['tstamp'] = df.iloc[:,0]
+df.sort_values('tstamp', ascending=True)
+check_time_gaps(df, {'symbol': s, 'interval': 3})
+df.set_index('tstamp', drop=True, inplace=True)
+
+# dataframe with all data
+dataset_name = str(df.index[-1]).replace(' ', 'T').replace(':', '-') + '.' + '_'.join(symbols) + '.' + str(len(df))
+#df.to_csv('/home/selknam/var/' +  dataset_name + '.csv')
+df.reset_index(drop=False, inplace=True)
+
+len(df.iloc[:,0].drop_duplicates()) == len(df.iloc[:,0])
+#%%
+#TRAIN
+
+# Y for regression
+y = df['AUDUSD_3_close_SROC_15'].shift(-15)
+
+# Y for classification
+df['YB_AUDUSD_3_close_SROC_15'] = 0
+df['YB_AUDUSD_3_close_SROC_15'] = df['YB_AUDUSD_3_close_SROC_15'].mask(y >  0.2, 1)
+
+df['YS_AUDUSD_3_close_SROC_15'] = 0
+df['YS_AUDUSD_3_close_SROC_15'] = df['YS_AUDUSD_3_close_SROC_15'].mask(y < -0.2, 1)
+
+
+df_train = df[df['tstamp'] < pd.to_datetime('2022-06-02T00:00:00')]
+df_test  = df[df['tstamp'] > pd.to_datetime('2022-06-02T00:00:00')]
+#%%
+import driverlessai
+
+address = 'http://192.168.3.114:12345'
+username = 'admin'
+password = 'admin'
+dai = driverlessai.Client(address = address, username = username, password = password)
+
+dataset_test   = dai.datasets.create(df_test,  name='test.'  + dataset_name)
+
+dataset_train  = dai.datasets.create(df_train, name='train.' + dataset_name)
+
+
+# %%
+ROCs = [15]
+fh = 15
+expert_settings = {
+    'imbalance_sampling_method': 'auto',
+    'included_models': ['ImbalancedLightGBM', 'LightGBM'],
+    'imbalance_sampling_threshold_min_rows_original': 1000
+}
+experiments = list()
+#BUY
+for roc in ROCs:
+    target_column = 'YB_AUDUSD_3_close_SROC_' + str(roc)
+    name = target_column.replace('AUDUSD_3_close_', 'v1.buy.') + '_FH' + str(fh) + '_T' + str(roc*3)
+    xp = dai.experiments.create_async(train_dataset=dataset_train,
+                                        task='classification',
+                                        name=name,
+                                        target_column=target_column,
+                                        time_column='tstamp',
+                                        num_prediction_periods=fh,
+                                        accuracy=7,
+                                        time=5,
+                                        interpretability=5,
+                                        config_overrides=None,
+                                        **expert_settings)
+    experiments.append(xp)
+
+#SELL
+for roc in ROCs:
+    target_column = 'YS_AUDUSD_3_close_SROC_' + str(roc)
+    name = target_column.replace('AUDUSD_3_close_', 'v1.sell.') + '_FH' + str(fh) + '_T' + str(roc*3)
+    xp = dai.experiments.create_async(train_dataset=dataset_train,
+                                        task='classification',
+                                        name=name,
+                                        target_column=target_column,
+                                        time_column='tstamp',
+                                        num_prediction_periods=fh,
+                                        accuracy=7,
+                                        time=5,
+                                        interpretability=5,
+                                        config_overrides=None,
+                                        **expert_settings)
+    experiments.append(xp)
+# %%
+#FORECAST
---
+++ old/model-gbm.py
@@ -0,0 +1,142 @@
+# %% 🐭
+from sys import argv
+from rata.utils import parse_argv, split_sequences
+
+fake_argv  = 'model-gbm.py --db_host=192.168.3.113 '
+fake_argv += '--symbol=EURUSD --interval=1 --shift=3 '
+fake_argv += '--X_symbols=EURUSD,AUDUSD,GBPUSD '
+fake_argv += '--X_include=vpt,rsi,stoch,others_cr,macd,kst,adx,cci,dch,* '
+fake_argv += '--X_exclude=volatility_kcli '
+
+fake_argv += '--nrows=3000 ' 
+fake_argv += '--test_lenght=800 '
+
+fake_argv += '--iterations=30 '
+fake_argv += '--learning_rate=0.3 '
+fake_argv += '--depth=3 '
+fake_argv += '--l2_leaf_reg=3 '
+fake_argv += '--loss_function=RMSE '
+
+fake_argv = fake_argv.split()
+argv = fake_argv #### *!
+_conf = parse_argv(argv=argv)
+
+_conf['X_symbols']   = _conf['X_symbols'].split(',')
+_conf['X_include']   = _conf['X_include'].split(',')
+_conf['X_exclude']   = _conf['X_exclude'].split(',')
+
+y_target  = _conf['symbol'] + '_' + str(_conf['interval'])
+y_target += '_y_close_SROC_' + str(_conf['shift'])
+y_target += '_shift-' + str(_conf['shift'])
+_conf['y_target'] = y_target
+
+_conf
+
+# %% Global imports
+import pandas as pd
+import numpy  as np
+from rata.ratalib import check_time_gaps
+from sqlalchemy import create_engine
+import datetime
+from sklearn.metrics import mean_squared_error
+
+#%%
+engine = create_engine('postgresql+psycopg2://rata:acaB.1312@' + _conf['db_host'] + ':5432/rata')
+
+df_join = pd.DataFrame()
+for s in _conf['X_symbols']:
+    sql =  "select * from feateng "
+    sql += "where symbol='" + s + "' and interval=" + str(_conf['interval'])
+    sql += " order by tstamp desc limit " + str(_conf['nrows'])
+    print(sql)
+    df = pd.read_sql_query(sql, engine).sort_values('tstamp')
+    X_prefix = s + '_' + str(_conf['interval']) + '_'
+    for c in df.columns:
+        df.rename({c: X_prefix + c}, axis=1, inplace=True)
+    
+    if len(df_join) == 0:
+        df_join = df
+    else:
+        print('Merge')
+        df_join = pd.merge(df_join, df, how='inner', left_on=df_join.columns[0], right_on=df.columns[0])
+
+df = df_join.copy()
+df.sort_values(df.columns[0])
+df['tstamp'] = df.iloc[:,0]
+df.sort_values('tstamp', ascending=True)
+check_time_gaps(df, {'symbol': s, 'interval': 3})
+df.set_index('tstamp', drop=True, inplace=True)
+
+print(len(df.iloc[:,0].drop_duplicates()) == len(df.iloc[:,0]))
+df.sort_values('tstamp', inplace=True)
+df[df.select_dtypes(np.float64).columns] = df.select_dtypes(np.float64).astype(np.float32)
+
+#%% Features handling
+ys_todelete = list()
+for c in df.columns:
+    if 'shift' in c:
+        ys_todelete.append(c)
+    if 'symbol' in c:
+        ys_todelete.append(c)
+
+df = df[:-9]
+#columns containing NaNs
+dfnans = pd.DataFrame(df.isnull().sum())
+nancols = list(dfnans[dfnans[0] > 0].index)
+
+X = df.drop(ys_todelete + ['EURUSD_1_symbol'] + nancols, axis=1)
+
+y = df[_conf['y_target']]
+X_train = X[:-_conf['test_lenght']]
+y_train = y[:-_conf['test_lenght']]
+
+X_test = X[-_conf['test_lenght']:]
+y_test = y[-_conf['test_lenght']:]
+
+#%%
+from catboost import CatBoostRegressor
+
+model = CatBoostRegressor(iterations=_conf['iterations'], 
+                          depth=_conf['depth'], 
+                          learning_rate=_conf['learning_rate'], 
+                          loss_function=_conf['loss_function'])
+
+t0 = datetime.datetime.now()
+model.fit(X_train, y_train)
+fit_time = int((datetime.datetime.now() - t0).total_seconds() / 60)
+#%%
+dfv  = pd.DataFrame(y_test)
+dfv.rename({y_target: 'y_test'}, axis=1)
+dfv['y_pred']    = model.predict(X_test)
+dfv['symbol']    = _conf['symbol']
+dfv['interval']  = _conf['interval']
+dfv['shift']     = _conf['shift']
+dfv['X_symbols'] = ','.join(_conf['X_symbols'])
+
+dfv['test_lenght']  = _conf['test_lenght']
+dfv['nrows']        = _conf['nrows']
+
+dfv['model_tstamp'] = df.index.max()
+dfv['model_id']     = str(df.index.max()).replace(' ', 'T')
+dfv['fit_time']     = fit_time
+
+dfv['iterations']    = _conf['iterations']
+dfv['learning_rate'] = _conf['learning_rate']
+dfv['depth']         = _conf['depth']
+dfv['l2_leaf_reg']   = _conf['l2_leaf_reg']
+
+dfv['mse']          = mean_squared_error(dfv['y_test'], dfv['y_pred'])
+# %% Feature importance
+dffi = pd.DataFrame(model.feature_names_)
+dffi.rename({0: 'feature_name'}, axis=1, inplace=True)
+dffi['feature_importance'] = model.feature_importances_
+dffi.sort_values('feature_importance', ascending=False, inplace=True)
+dffi.head(45)
+
+#%%
+engine = create_engine('postgresql+psycopg2://rata:acaB.1312@' + _conf['db_host'] + ':5432/rata')
+df_val.reset_index().to_sql('catboost', engine, if_exists='append', index=False)
+
+# %%
+df_val
+# %%
