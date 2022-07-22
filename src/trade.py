a2d4e5b733ef60bfdf1031413838de0566d0af1d
Fernando Morales <fernando@fmorales.cl>
2022-07-22 02:19:40 +0000

    sync

--- src/trade.py
+++ src/trade.py
@@ -1,5 +1,6 @@
 # %% 🐭
 from sys import argv
+from this import d
 from rata.utils import parse_argv
 
 fake_argv  = 'trade.py --db_host=192.168.3.113 '
@@ -35,17 +36,100 @@ from sqlalchemy import create_engine
 import datetime
 
 engine = create_engine('postgresql+psycopg2://rata:acaB.1312@' + _conf['db_host'] + ':5432/rata')
-#%% ###                 SECTION CREATE MODEL          ###
 #%%
-
-
-
 sql =  "select * from predict_catboost "
-sql += "where symbol='" + s + "' and interval=" + str(_conf['interval'])
+#sql += "where symbol='" + _conf['symbol']+ "' and interval=" + str(_conf['interval'])
+sql += " where tstamp > (now() - interval '12 hours')"
 sql += " order by tstamp desc "
+
+sql = """
+with
+p as
+(select tstamp, symbol, "interval", shift, avg(y_pred) y_pred_avg
+from predict_catboost 
+group by  tstamp, symbol, "interval", shift
+order by tstamp desc),
+t as
+(select tstamp_test, avg(y_current) y_current_avg, y_test, symbol, "interval", shift 
+from predict_catboost
+--where y_current = y_test --NaNs ??? :(
+group by tstamp_test, y_test, symbol, "interval", shift
+order by tstamp_test desc )
+select p.tstamp, t.tstamp_test, y_current_avg, y_test, y_pred_avg, t.symbol, t.interval, t.shift
+from p inner join t
+  on 
+    p.tstamp = t.tstamp_test and
+    p.symbol = t.symbol and
+    p.interval = t.interval and
+    p.shift = t.shift
+where tstamp > (now() - interval '24 hours')
+order by tstamp desc
+"""
+
 print(sql)
 df = pd.read_sql_query(sql, engine).sort_values('tstamp')
+df.set_index('tstamp', inplace=True)
+df
+# %%
+q = 0.0
+df11 = df[(df['interval'] == 1) & (df['shift'] == 1)]
+df11 = df11[df11['y_pred'] > df11['y_pred'].quantile(q)]
+df13 = df[(df['interval'] == 1) & (df['shift'] == 3)]
+df13 = df13[df13['y_pred'] > df13['y_pred'].quantile(q)]
+df16 = df[(df['interval'] == 1) & (df['shift'] == 6)]
+df16 = df16[df16['y_pred'] > df16['y_pred'].quantile(q)]
+df19 = df[(df['interval'] == 1) & (df['shift'] == 9)]
+df19 = df19[df19['y_pred'] > df19['y_pred'].quantile(q)]
+df31 = df[(df['interval'] == 3) & (df['shift'] == 1)]
+df31 = df31[df31['y_pred'] > df31['y_pred'].quantile(q)]
+df33 = df[(df['interval'] == 3) & (df['shift'] == 3)]
+df33 = df33[df33['y_pred'] > df33['y_pred'].quantile(q)]
+df36 = df[(df['interval'] == 3) & (df['shift'] == 6)]
+df36 = df36[df36['y_pred'] > df36['y_pred'].quantile(q)]
+df39 = df[(df['interval'] == 3) & (df['shift'] == 9)]
+df39 = df39[df39['y_pred'] > df39['y_pred'].quantile(q)]
+dfB = pd.concat([df11, df13, df16, df19, df31, df33, df39]).sort_index()
+q = 1.0
+df11 = df[(df['interval'] == 1) & (df['shift'] == 1)]
+df11 = df11[df11['y_pred'] < df11['y_pred'].quantile(q)]
+df13 = df[(df['interval'] == 1) & (df['shift'] == 3)]
+df13 = df13[df13['y_pred'] < df13['y_pred'].quantile(q)]
+df16 = df[(df['interval'] == 1) & (df['shift'] == 6)]
+df16 = df16[df16['y_pred'] < df16['y_pred'].quantile(q)]
+df19 = df[(df['interval'] == 1) & (df['shift'] == 9)]
+df19 = df19[df19['y_pred'] < df19['y_pred'].quantile(q)]
+df31 = df[(df['interval'] == 3) & (df['shift'] == 1)]
+df31 = df31[df31['y_pred'] < df31['y_pred'].quantile(q)]
+df33 = df[(df['interval'] == 3) & (df['shift'] == 3)]
+df33 = df33[df33['y_pred'] < df33['y_pred'].quantile(q)]
+df36 = df[(df['interval'] == 3) & (df['shift'] == 6)]
+df36 = df36[df36['y_pred'] < df36['y_pred'].quantile(q)]
+df39 = df[(df['interval'] == 3) & (df['shift'] == 9)]
+df39 = df39[df39['y_pred'] < df39['y_pred'].quantile(q)]
+dfS = pd.concat([df11, df13, df16, df19, df31, df33, df39]).sort_index()
+# %%
+dfB
+# %%
+dfS
+# %%
+df_pred = df[['y_pred', 'symbol', 'interval', 'shift']].reset_index()
+df_test = df[['tstamp_test', 'y_current', 'symbol', 'interval', 'shift']].reset_index(drop=True).rename({'tstamp_test': 'tstamp'}, axis=1)
+df_pred.merge(df_test, on=['tstamp', 'symbol', 'interval', 'shift'], how='inner')#.sort_index().tail(60)
+# %%
+df_pred
+#df_test
 
-#%% 
-df['y_pred'].min()
+# %%
+df_test
+# %%
+df11 = df[(df['interval'] == 1) & (df['shift'] == 1)]
+df13 = df[(df['interval'] == 1) & (df['shift'] == 3)]
+df16 = df[(df['interval'] == 1) & (df['shift'] == 6)]
+df19 = df[(df['interval'] == 1) & (df['shift'] == 9)]
+df31 = df[(df['interval'] == 3) & (df['shift'] == 1)]
+df33 = df[(df['interval'] == 3) & (df['shift'] == 3)]
+df36 = df[(df['interval'] == 3) & (df['shift'] == 6)]
+df39 = df[(df['interval'] == 3) & (df['shift'] == 9)]
+df11
+
 # %%
