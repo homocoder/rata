e4421c0c0c7a4b94bf4f62be996403715dbaf74d
Fernando Morales <fernando@fmorales.cl>
2022-09-01 20:04:11 +0000

    start on pewenche server

--- old/forecasts_binclf_launcher.bash
+++
@@ -1,9 +0,0 @@
-interval=$1
-sleep 10
-cd /home/selknam/dev/rata/src && \
-  source /home/selknam/opt/miniconda3/bin/activate rata.py310 && \
-  python -u /home/selknam/dev/rata/src/forecasts_binclf_launcher.py \
-    --db_conf=conf/db.json \
-    --symbol_conf=conf/rates_launcher.$interval.json \
-    --forecasts_binclf_conf=conf/forecasts_binclf.json \
-    #--forecast_datetime=2021-12-27T11:20:05 
